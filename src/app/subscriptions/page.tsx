6ec6e2c8703b1613a478e225e7a15c910902766b
Risbel Suarez <risbel961019@gmail.com>
2024-03-08 10:15:07 -0500

    feat: Add movile view on `/subscriptions`

--- src/app/subscriptions/page.tsx
+++ src/app/subscriptions/page.tsx
@@ -13,19 +13,19 @@ const Subscriptions = async () => {
   const subscriptions: ISubscriptions = await res.json();
 
   return (
-    <div className="p-16">
+    <div className="p-4 lg:p-16">
       <div className="flex">
         <Link className="flex bg-yellow-400 hover:bg-yellow-300 pr-2 py-2 rounded-lg shadow-md" href={"/"}>
           <ChevronLeft /> <span className="font-semibold">Go back</span>
         </Link>
       </div>
 
-      <div className="p-8">
+      <div className="flex flex-col items-center p-8">
         <h1 className="text-3xl font-semibold">Subscriptors:</h1>
         <div className="flex flex-wrap gap-4 py-8">
           {subscriptions.results.map((item) => {
             return (
-              <div className="border p-4 rounded-xl" key={item.id}>
+              <div className="w-full lg:w-auto border p-4 rounded-xl" key={item.id}>
                 <p className="font-semibold text-xl">👤 {item.name}</p>
                 <p>✉️ {item.email}</p>
                 <p>📞 {item.phone}</p>
